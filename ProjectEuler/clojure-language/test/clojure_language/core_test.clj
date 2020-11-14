b76589af5338895a28e7075d2e23fa603ab26bab
Arthur Lacerda <arthur.lacerda@solvimm.com>
2020-11-14 02:32:33 -0300

    remove project unused dirs

--- ProjectEuler/clojure-language/test/clojure_language/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-language.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-language.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
